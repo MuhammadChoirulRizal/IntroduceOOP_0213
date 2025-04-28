a716d2e1098c4429edf031305c95d21a3bcc8fd2
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-04-28 19:14:08 +0700

    membuat OOP

---
+++ coba2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class persegipanjang
+{
+public:
+    float panjang;
+    float lebar;
+    float luas;
+    void inputData()
+    {
+        cout << " input panjang: ";
+        cin >> panjang;
+        cout << " input lebar :";
+        cin >> lebar;
+    }
+    void menghitungluas()
+    {
+        luas = panjang * lebar;
+    }
+    void tampilhasil()
+    {
+        cout << "luas persegi panjang =" << luas << endl;
+    }
+};
+
+int main()
+{
+    persegipanjang per;
+    per.inputData();
+    per.menghitungluas();
+    per.tampilhasil();
+    return 0;
+}
